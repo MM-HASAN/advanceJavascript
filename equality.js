f631a64e90d7055306219799998d8856e9d13293
MM-HASAN <2020.mmhasan@gmail.com>
2021-02-10 07:29:13 +0600

    equality === vs == practice

---
+++ equality.js
@@ -0,0 +1,11 @@
+// const firstNum = 2;
+// const secondNum = "2";
+
+const firstZero = 1;
+const secondFalse = true;
+
+if(firstZero == secondFalse){
+  console.log(true)
+}else{
+  console.log(false)
+}
